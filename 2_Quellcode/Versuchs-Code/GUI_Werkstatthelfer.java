8db51bd5b91dea56f1ad7eab28e24dece6dbaa23
HofmannRibin <robin.erhard.hofmann@hs-furtwangen.de>
2022-05-30 19:35:15 +0200

    Dateien aktualisiert
    Test-Dateien entfernt und Vorlage für GUI eingefügt

---
+++ 2_Quellcode/Versuchs-Code/GUI_Werkstatthelfer.java
@@ -0,0 +1,40 @@
+
+public class GUI_Werkstatthelfer {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Erstellen und Anpassen von JLabel (Textfelder)
+		
+		
+		
+		//Erstellen und Anpassen von JComboBox (Pull-Down-Menü, Material und Bearbeitung)
+		
+		
+		
+		//Erstellen und Anpassen von JS (Klicker, Radius und Oberfläche)
+		
+		
+		
+		//Erstellen und Anpassen von JMenuBar (Menü-Bar)
+		
+		
+		
+		//Erstellen und Anpassen von JFrame (Gesamtfenster)
+		
+		
+		
+		//Einfügen aller Komponenten in den JFrame
+		
+		
+		
+		//Erstellen und Anpassen aller Listener (public void ....)
+		
+		
+		
+		
+
+	}
+
+}
