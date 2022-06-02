5ba213553edc12a045724d4b766277a60d29d7be
vinzenzdreher <101865609+vinzenzdreher@users.noreply.github.com>
2022-06-02 12:18:20 +0200

    Datenbank

--- 2_Quellcode/Versuchs-Code/ConnectApp/src/net/codejava/CuttingPlateManager.java
+++ 2_Quellcode/Versuchs-Code/ConnectApp/src/net/codejava/CuttingPlateManager.java
@@ -1,28 +1,70 @@
 package net.codejava;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 
 public class CuttingPlateManager {
-
-	public static void main(String[] args) {
+	
+	private String databaseURL;
+	private String material; 
+	private String beaumfang;
+	private String radius; 
+	private String abfrage;
+	private int a;
+	
+	
+	
+	
+	CuttingPlateManager(){
+		databaseURL = "jdbc:ucanaccess://cuttingplatedatabase.accdb" ;
+		a=1;
 		
-		String databaseURL = "jdbc:ucanaccess://cuttingplatedatabase.accdb";
-		
+	}
+	public void getConnection(){
 		try {
-			Connection connection = DriverManager.getConnection(databaseURL);
 			
-			System.out.println("succesfully connected");
+			Connection con = DriverManager.getConnection(databaseURL);
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery(abfrage);
 			
-			connection.close();
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+		}
+		catch(SQLException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	
+	public void getSchneidplatte(){
+	
+		String abfrage = "SELECT * FROM cuttingplatedatabase WHERE Material = material AND Radius = radius AND Bearbeitungsumfang = beaumfang";
+		getConnection();
+		
+		
+		return;
+		{
+		while(rs.next()) {
+			System.out.println(rs.getString(a));
+			a++;
+			if(a==3) {
+				break;
+			}
+			
+		}
 		}
 		
 		
 	}
+	
 
-}
+public static void main(String[]args) {
+	CuttingPlateManager cut1 = new CuttingPlateManager();
+	cut1.getConnection();
+	cut1.getSchneidplatte();
+	
+}}
+
+
+		
+	
